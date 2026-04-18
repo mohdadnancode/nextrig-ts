ae980753dd1c6c4b12f52fdf4b3d9407a43aeba5
Adnan <mohdadnan.code@gmail.com>
2026-04-18 10:39:37 +0530

    server connected

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// connect database
+connectDB();
+
+// middleware
+// app.use(cors());
+app.use(express.json());
+
+// routes
+app.get("/test-db", async (req, res) => {
+    try {
+        const collections = await mongoose.connection.db.listCollections().toArray();
+        res.json({
+            status: "connected",
+            collections
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            error: error.message
+        });
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is runnnig on port ${PORT}`);
+});
